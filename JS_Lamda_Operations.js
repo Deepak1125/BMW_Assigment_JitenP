087f06771317377d4b0580bc3068a34bafd191f3
Deepak Subbarao <deepak.subbarao@bmwtechworks.in>
2026-07-27 16:04:15 +0530

    doing task2

--- JS_Lamda_Operations.js
+++ JS_Lamda_Operations.js
@@ -4,5 +4,17 @@
 
 let names = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Isabella", "Jack"];
 
+//for Each Method
 names.forEach(name => console.log(name));
 
+//for Map Function
+let Shockednames = names.map(name => name+'!');
+console.log(Shockednames);
+
+//for filter Function
+let nameslength = names.filter(name => name.length>5);
+console.log(nameslength);
+
+//for find Function
+let namefinder = names.find(name => name.startsWith('D'));
+console.log(namefinder);
